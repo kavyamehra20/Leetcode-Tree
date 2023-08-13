9ed48b15f7141c693a510a5ffe03c31ac03bcf33
Kavya Mehra <60140554+kavyamehra20@users.noreply.github.com>
2023-08-13 16:26:18 +0530

    Same Tree

--- Sametree.java
+++ Sametree.java
@@ -1,21 +1,27 @@
-class Solution {
+class Sametree {
 
     public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
-}
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null || q==null)
-            return (p==q);
-        return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+        if (p == null || q == null)
+            return (p == q);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
