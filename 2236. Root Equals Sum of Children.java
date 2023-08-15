f4b0d88d13b26ee47dbb18ae215e92d55217f3b5
Kavya Mehra <60140554+kavyamehra20@users.noreply.github.com>
2023-08-15 15:43:56 +0530

    DAY 3

---
+++ 2236. Root Equals Sum of Children.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean checkTree(TreeNode root) {
+        return root.left.val + root.right.val==root.val ? true : false;
+    }
+
+}
